509f17d3a9cc15efbad4dd6675519e5fa34cf003
Eric Bumbalough <eric.bumbalough@gmail.com>
2021-08-30 19:47:00 -0400

    simplified model

--- LearnObjPlanApp/admin.py
+++ LearnObjPlanApp/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Course, Origin, ContentArea, Objective
+from .models import Assessment, Learning_Activity, Objective, Course
 
-admin.site.register(Course)
-admin.site.register(Origin)
-admin.site.register(ContentArea)
 admin.site.register(Objective)
+admin.site.register(Assessment)
+admin.site.register(Learning_Activity)
+admin.site.register(Course)
--- LearnObjPlanApp/models.py
+++ LearnObjPlanApp/models.py
@@ -1,22 +1,19 @@
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.deletion import CASCADE
 
 # TODO these will come from locale files
-# TODO make these specific to objectives or content areas
+
+parents_help = ('Is this requirement devirived different requirements?'
+                + ' If so, put that requirement here. If not, leave blank.'
+                + ' If you input parents, the system will automatically add '
+                + 'childern when you save.')
+children_help = ('Are other requirements derived from this one? '
+                 + 'If so, put them here. If not, leave blank.'
+                 + ' If you input children, the system will automatically add '
+                 + 'parents when you save.')
 course_name_help = 'Example: College Algebra'
 course_number_help = 'Example: MATH 136'
-origin_help = ('Where did this requirement come from? Common sources are '
-               + 'syllabi, accreditors, or institutional objectives. Leave '
-               + 'blank for children.')
-origin_abbr_help = 'Shortened name or acryonym for origin.'
-origin_url_help = 'Webpage where the origin is found.'
-origin_number_help = ('The number for objective from the origin. '
-                      + 'Leave blank if there is no origin or the origin does '
-                      + 'not list a number.')
-parents_help = ('Is this requirement devirived different requirements?'
-                + ' If so, put that requirement here. If not, leave blank.')
-children_help = ('Are other requirements derived from this one? '
-                 + 'If so, put them here. If not, leave blank')
-
 
 class Course(models.Model):
     name = models.CharField(max_length=200, help_text=course_name_help)
@@ -25,32 +22,27 @@ class Course(models.Model):
     def __str__(self):
         return self.number
 
-
-class Origin(models.Model):
-    origin = models.CharField(max_length=200, help_text=origin_help)
-    abbreviation = models.CharField(max_length=200, blank=True,
-                                    help_text=origin_abbr_help)
-    url = models.URLField(blank=True, help_text=origin_url_help)
-
+class Learning_Activity(models.Model):
+    learning_activity = models.CharField(max_length=100)
+    course = models.ForeignKey(Course, on_delete=CASCADE)
+    descripton = models.TextField(blank=True) 
+    
     def __str__(self):
-        return self.origin
-
-
-class ContentArea(models.Model):
-    description = models.TextField()
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True,
-                               null=True, help_text=parents_help)
-    origin = models.ForeignKey(Origin, on_delete=models.CASCADE, blank=True,
-                               null=True, help_text=origin_help)
-
+        return self.learning_activity
+    
+    
+class Assessment(models.Model):
+    assessment = models.CharField(max_length=100)
+    course = models.ForeignKey(Course, on_delete=CASCADE)
+    descripton = models.TextField(blank=True) 
+    
     def __str__(self):
-        return self.description
+        return self.assessment
 
 
 class Objective(models.Model):
-    objective_text = models.TextField()
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    objective = models.CharField(max_length=100)
+    course = models.ForeignKey(Course, on_delete=CASCADE)
     parents = models.ManyToManyField('self',
                                      blank=True,
                                      help_text=parents_help,
@@ -61,13 +53,18 @@ class Objective(models.Model):
                                       help_text=children_help,
                                       symmetrical=False,
                                       related_name='children_related')
-    origin = models.ForeignKey(Origin, on_delete=models.CASCADE, blank=True,
-                               null=True, help_text=origin_help)
-    origin_number = models.CharField(max_length=200, blank=True,
-                                     help_text=origin_number_help)
-    content_area = models.ForeignKey(ContentArea, on_delete=models.CASCADE,
-                                     blank=True, null=True)
+    assessments = models.ForeignKey(Assessment,
+                                    on_delete=models.CASCADE,
+                                    blank=True,
+                                    null=True)
+    learning_activities = models.ForeignKey(Learning_Activity,
+                                            on_delete=models.CASCADE,
+                                            blank=True,
+                                            null=True)
     note = models.TextField(blank=True)
 
     def __str__(self):
-        return self.objective_text
+        return self.objective
+    
+    
+
