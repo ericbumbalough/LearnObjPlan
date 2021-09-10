051ce2bb0adf23a4e55020848dd457a8e50c9a08
Eric Bumbalough <eric.bumbalough@gmail.com>
2021-09-10 11:09:37 -0400

    objective list view

--- LearnObjPlanApp/urls.py
+++ LearnObjPlanApp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('<int:objective_id>/', views.objective_list, name='objective_list')
+    path('', views.ObjectiveList.as_view(), name='objective_list')
 ]
--- LearnObjPlanApp/views.py
+++ LearnObjPlanApp/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import HttpResponse, render
-
+from django.views import generic
 from .models import Objective
+from Accounts.models import CustomUser as User
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-def objective_list(request, objective_id):
-    context = {
-        'objective': Objective.objects.get(id=objective_id)
-    }
-    return render(request, 'LearnObjPlanApp/objective.html', context)
+class ObjectiveList(LoginRequiredMixin, generic.ListView):
+    '''List of all objectives for user'''
+    model = Objective
+    template_name = 'objective_list.html'
+    
+    def get_queryset(self):
+        return Objective.objects.filter(user=self.request.user)
